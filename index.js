85550bb1ba2585a204487e91cbbf9eeed468546c
zeena kareem <zena34269@gmail.com>
2020-05-19 14:00:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let element = document.querySelector("body");
+element.removeChild(document.querySelector("main#main"))
+
+let newHeader=document.querySelector("h1#victory")
+newHeader.innerHTML='is the champion'
