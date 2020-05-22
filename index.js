3878c414477b498d841030cca6b35f1ab3d10577
zeena kareem <zena34269@gmail.com>
2020-05-22 18:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ let element = document.querySelector("body");
 element.removeChild(document.querySelector("main#main"))
 
 let newHeader=document.querySelector("h1#victory")
+<<<<<<< HEAD
 newHeader.innerHTML='is the champion'
+=======
+newHeader.innerHtml='is the champion'
+>>>>>>> 127aa632feb340853e2ca6b225cb7e3c7c77d5c3
